8004dad50fcb711a6f620ea24f5dddbaff61ebcf
mhodali <79901571+mhodali@users.noreply.github.com>
2021-03-14 13:07:37 +0200

    Update create-client.sh

--- create-client.sh
+++ create-client.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
-docker build /home/task3/client/ -t client
-if [ "$(docker ps -a | grep client)" ]; then
-  docker stop $(docker ps -a | grep client |awk '{print  $1}')
-  docker rm $(docker ps -a | grep client |awk '{print  $1}')
+#docker build /home/task3/client/ -t mhodali/client
+if [ "$(docker ps -a | grep mhodali/client)" ]; then
+  docker stop $(docker ps -a | grep mhodali/client |awk '{print  $1}')
+  docker rm $(docker ps -a | grep mhodali/client |awk '{print  $1}')
 fi
-docker run -dit --name client -p 80:80  -t client
+docker run -dit --name client -p 80:80  -t mhodali/client
 
